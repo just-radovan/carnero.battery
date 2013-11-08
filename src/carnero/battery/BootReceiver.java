f34848779153d00c10ed1e5b2de328064cb202a6
carnero <carnero@carnero.cc>
2013-11-08 19:00:11 +0100

    BOOT

---
+++ src/carnero/battery/BootReceiver.java
@@ -0,0 +1,14 @@
+package carnero.battery;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		final Intent serviceIntent = new Intent(context, Main.class);
+		context.startService(serviceIntent);
+	}
+}
